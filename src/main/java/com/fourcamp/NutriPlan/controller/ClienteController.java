fac1604b123c2001591bb444fbda7e99ac7394b5
wmatos <willianssantos227@gmail.com>
2024-08-15 17:14:09 -0300

    Atualizado script base de dados, e function meta

--- src/main/java/com/fourcamp/NutriPlan/controller/ClienteController.java
+++ src/main/java/com/fourcamp/NutriPlan/controller/ClienteController.java
@@ -1,139 +1,134 @@
-/*package com.fourcamp.NutriPlan.controller;
-
-import com.fourcamp.NutriPlan.dto.*;
-import com.fourcamp.NutriPlan.dto.conta.ClienteDto;
-import com.fourcamp.NutriPlan.model.conta.Cliente;
-import com.fourcamp.NutriPlan.service.conta.ClienteService;
-import com.fourcamp.NutriPlan.service.meta.MetaService;
-import com.fourcamp.NutriPlan.utils.JwtUtils;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
-
-@RestController
-public class ClienteController {
-
-    @Autowired
-    private ClienteService clienteService;
-
-    @Autowired
-    private MetaService metaService;
-
-    @Autowired
-    private JwtUtils jwtUtils;
-
-    @PostMapping("/create/account")
-    @Operation(description = "Criação da conta")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Criação da conta com sucesso"),
-            @ApiResponse(responseCode = "400", description = "Falha na criação da conta, dados inválidos ou formato incorreto"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
-    public ResponseEntity<String> addCliente(@RequestBody Cliente cliente) {
-        Date dataSql = new Date(cliente.getDataNascimento().getTime());
-        String mensagem = clienteService.criarCliente(
-                cliente.getNome(),
-                cliente.getEmail(),
-                cliente.getGenero(),
-                cliente.getPeso(),
-                cliente.getAltura(),
-                dataSql,
-                cliente.getSenha(),
-                cliente.getCategoria()
-        );
-
-        return ResponseEntity.ok().body(mensagem);
-    }
-
-    @PostMapping("/login")
-    @Operation(description = "Fazer o login da conta")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Login com sucesso"),
-            @ApiResponse(responseCode = "400", description = "Falha no login, dados inválidos ou formato incorreto"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
-    public String login(@RequestBody LoginRequestDto loginRequest) {
-        return clienteService.login(loginRequest.getEmail(), loginRequest.getSenha());
-    }
-
-    @PostMapping("/atualizar-peso")
-    @Operation(description = "Atualiza o peso do cliente")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Atualização do peso com sucesso"),
-            @ApiResponse(responseCode = "400", description = "Falha na atualização do peso, dados inválidos ou formato incorreto"),
-            @ApiResponse(responseCode = "401", description = "Não autorizado, token inválido"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
-    public ResponseEntity<String> alterarPeso(@RequestHeader("Authorization") String token, @RequestBody PesoDto novoPeso) {
-        String mensagem = clienteService.alterarPeso(email, novoPeso.getNovoPeso());
-        return ResponseEntity.ok(mensagem);
-    }
-
-    @PostMapping("/formulario")
-    @Operation(description = "Formulario do objetivo")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Formulario respondido com sucesso"),
-            @ApiResponse(responseCode = "400", description = "Falha no preenchimento do formulario, dados inválidos ou formato incorreto"),
-            @ApiResponse(responseCode = "401", description = "Não autorizado, token inválido"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
-    public ResponseEntity<String> formularioObjetivo(@RequestHeader("Authorization") String token, @RequestBody ClienteDto cliente) {
-
-        String mensagem = clienteService.formularioObjetivo(email, cliente.getCategoria(), cliente.getTempoMeta());
-        return ResponseEntity.ok(mensagem);
-    }
-
-    @PostMapping("/Geb")
-    @Operation(description = "Visualizar o gasto energético basal do cliente logado")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Retorno do GET"),
-            @ApiResponse(responseCode = "400", description = "Falha no retorno do GET, dados inválidos ou formato incorreto"),
-            @ApiResponse(responseCode = "401", description = "Não autorizado, token inválido"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
-    public ResponseEntity<Double> visualizarTMB(@RequestHeader("Authorization") String token, @RequestBody CategoriaAtividadeRequestDto request) {
-        JwtData jwtData = clienteService.obterDadosDoUsuario(email);
-
-        return ResponseEntity.ok(metaService.calcularGETSalvar(
-                email,
-                jwtData.getCategoria(),
-                jwtData.getTempoMeta(),
-                jwtData.getGenero(),
-                jwtData.getPeso(),
-                jwtData.getAltura(),
-                jwtData.getDataNascimento(),
-                request.getCategoriaAtividade()
-        ));
-    }
-
-    @GetMapping("acessar-plano")
-    @Operation(description = "Visualizar o plano nutricional do cliente")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Retorno do plano nutricional"),
-            @ApiResponse(responseCode = "400", description = "Falha no retorno do plano nutricional, dados inválidos ou formato incorreto"),
-            @ApiResponse(responseCode = "401", description = "Não autorizado, token inválido"),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
-    public ResponseEntity<String> acessarPlano(@RequestHeader("Authorization") String token,
-                                               @RequestParam("categoriaAtividade") String categoriaAtividade) {
-        JwtData jwtData = clienteService.obterDadosDoUsuario(email);
-
-        String mensagem = metaService.acessarPlano(
-                email,
-                jwtData.getCategoria(),
-                jwtData.getTempoMeta(),
-                jwtData.getGenero(),
-                jwtData.getPeso(),
-                jwtData.getAltura(),
-                jwtData.getDataNascimento(),
-                categoriaAtividade
-        );
-
-        return ResponseEntity.ok(mensagem);
-    }
-}*/
+//package com.fourcamp.NutriPlan.controller;
+//
+//import com.fourcamp.NutriPlan.dto.*;
+//import com.fourcamp.NutriPlan.dto.conta.ClienteDto;
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.responses.ApiResponse;
+//import io.swagger.v3.oas.annotations.responses.ApiResponses;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//
+//@RestController
+//public class ClienteController {
+//
+//    @Autowired
+//    private ClienteService clienteService;
+//
+//    @Autowired
+//    private MetaService metaService;
+//
+////    @Autowired
+////    private JwtUtils jwtUtils;
+////
+////    @PostMapping("/create/account")
+////    @Operation(description = "Criação da conta")
+////    @ApiResponses(value = {
+////            @ApiResponse(responseCode = "200", description = "Criação da conta com sucesso"),
+////            @ApiResponse(responseCode = "400", description = "Falha na criação da conta, dados inválidos ou formato incorreto"),
+////            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+////    })
+////    public ResponseEntity<String> addCliente(@RequestBody Cliente cliente) {
+////        Date dataSql = new Date(cliente.getDataNascimento().getTime());
+////        String mensagem = clienteService.criarCliente(
+////                cliente.getNome(),
+////                cliente.getEmail(),
+////                cliente.getGenero(),
+////                cliente.getPeso(),
+////                cliente.getAltura(),
+////                dataSql,
+////                cliente.getSenha(),
+////                cliente.getCategoria()
+////        );
+////
+////        return ResponseEntity.ok().body(mensagem);
+////    }
+////
+////    @PostMapping("/login")
+////    @Operation(description = "Fazer o login da conta")
+////    @ApiResponses(value = {
+////            @ApiResponse(responseCode = "200", description = "Login com sucesso"),
+////            @ApiResponse(responseCode = "400", description = "Falha no login, dados inválidos ou formato incorreto"),
+////            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+////    })
+////    public String login(@RequestBody LoginRequestDto loginRequest) {
+////        return clienteService.login(loginRequest.getEmail(), loginRequest.getSenha());
+////    }
+//
+//    @PostMapping("/atualizar-peso")
+//    @Operation(description = "Atualiza o peso do cliente")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "Atualização do peso com sucesso"),
+//            @ApiResponse(responseCode = "400", description = "Falha na atualização do peso, dados inválidos ou formato incorreto"),
+//            @ApiResponse(responseCode = "401", description = "Não autorizado, token inválido"),
+//            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+//    })
+//    public ResponseEntity<String> alterarPeso(@RequestHeader("Authorization") String token, @RequestBody PesoDto novoPeso) {
+//        String mensagem = clienteService.alterarPeso(email, novoPeso.getNovoPeso());
+//        return ResponseEntity.ok(mensagem);
+//    }
+//
+//    @PostMapping("/formulario")
+//    @Operation(description = "Formulario do objetivo")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "Formulario respondido com sucesso"),
+//            @ApiResponse(responseCode = "400", description = "Falha no preenchimento do formulario, dados inválidos ou formato incorreto"),
+//            @ApiResponse(responseCode = "401", description = "Não autorizado, token inválido"),
+//            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+//    })
+//    public ResponseEntity<String> formularioObjetivo(@RequestHeader("Authorization") String token, @RequestBody ClienteDto cliente) {
+//
+//        String mensagem = clienteService.formularioObjetivo(email, cliente.getCategoria(), cliente.getTempoMeta());
+//        return ResponseEntity.ok(mensagem);
+//    }
+//
+//    @PostMapping("/Geb")
+//    @Operation(description = "Visualizar o gasto energético basal do cliente logado")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "Retorno do GET"),
+//            @ApiResponse(responseCode = "400", description = "Falha no retorno do GET, dados inválidos ou formato incorreto"),
+//            @ApiResponse(responseCode = "401", description = "Não autorizado, token inválido"),
+//            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+//    })
+//    public ResponseEntity<Double> visualizarTMB(@RequestHeader("Authorization") String token, @RequestBody CategoriaAtividadeRequestDto request) {
+//        JwtData jwtData = clienteService.obterDadosDoUsuario(email);
+//
+////        return ResponseEntity.ok(metaService.calcularGETSalvar(
+//                email,
+//                jwtData.getCategoria(),
+//                jwtData.getTempoMeta(),
+//                jwtData.getGenero(),
+//                jwtData.getPeso(),
+//                jwtData.getAltura(),
+//                jwtData.getDataNascimento(),
+//                request.getCategoriaAtividade()
+//        ));
+//    }
+//
+//    @GetMapping("acessar-plano")
+//    @Operation(description = "Visualizar o plano nutricional do cliente")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "Retorno do plano nutricional"),
+//            @ApiResponse(responseCode = "400", description = "Falha no retorno do plano nutricional, dados inválidos ou formato incorreto"),
+//            @ApiResponse(responseCode = "401", description = "Não autorizado, token inválido"),
+//            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+//    })
+//    public ResponseEntity<String> acessarPlano(@RequestHeader("Authorization") String token,
+//                                               @RequestParam("categoriaAtividade") String categoriaAtividade) {
+//        JwtData jwtData = clienteService.obterDadosDoUsuario(email);
+//
+//        String mensagem = metaService.acessarPlano(
+//                email,
+//                jwtData.getCategoria(),
+//                jwtData.getTempoMeta(),
+//                jwtData.getGenero(),
+//                jwtData.getPeso(),
+//                jwtData.getAltura(),
+//                jwtData.getDataNascimento(),
+//                categoriaAtividade
+//        );
+//
+//        return ResponseEntity.ok(mensagem);
+//    }
+//}
--- src/main/java/com/fourcamp/NutriPlan/service/conta/ClienteService.java
+++ src/main/java/com/fourcamp/NutriPlan/service/conta/ClienteService.java
@@ -35,93 +35,93 @@
 //    @Autowired
 //    private CalculoIdade calculoIdade;
 //
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
-//
-//    @Autowired
-//    private JwtUtils jwtUtils;
-//
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
-//
-//    public String criarCliente(String nome, String email, String genero, Double peso, Double pesoDesejado, Double altura, Date dataNascimento, String senha, String categoria, String tempoMeta){
-//
-//        if (!EmailValidador.isValidEmail(email)) {
-//            throw new EmailException();
-//        }
-//
-//        if (!SenhaValidador.isValidPassword(senha)) {
-//            throw new SenhaException();
-//        }
-//
-//        LocalDate dataNascimentoLocalDate = calculoIdade.convertToLocalDate(dataNascimento);
-//        int idade = calculoIdade.calcularIdade(dataNascimentoLocalDate);
-//        if (idade < 18) {
-//            throw new DataException();
-//        }
-//
-//        //Codifica a senha antes de salvar no banco de dados
-//        String encodedPassword = passwordEncoder.encode(senha);
-//
-//        clienteDao.criarCliente(nome, email, genero, peso, pesoDesejado, altura, dataNascimento, encodedPassword, categoria , tempoMeta);
-//        return Constantes.MSG_CRIACAO_CLIENTE_SUCESSO;
-//    }
-//
-//    /**
-//     * Autentica o usuário utilizando Spring Security e retorna um token JWT
-//     *
-//     * @param email o email do usuário
-//     * @param senha a senha do usuário
-//     * @return um token JWT válido ou null se a autenticação falhar
-//     */
-//    public String login(String email, String senha) {
-//        Authentication authentication = authenticationManager.authenticate(
-//                new UsernamePasswordAuthenticationToken(email, senha));
-//
-//            //Define o contexto de segurança com a autenticação bem-sucedida
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
-//
-//        //Gera o token JWT utilizando as informaçoes do usuário autenticado
-//        String jwt = jwtUtils.generateJwtToken(authentication);
-//
-//        return jwt;
-//    }
-//
-//    public String alterarPeso(String email, double novoPeso) {
-//        Cliente cliente = clienteDao.buscarClientePorEmail(email);
-//
-//        cliente.setPeso(novoPeso);
-//        clienteDao.atualizarPesoCliente(email, novoPeso);
-//        return Constantes.MSG_PESO_ALTERADO_SUCESSO;
-//    }
-//
-//    public String formularioObjetivo(String email, String categoria, String tempoMeta) {
-//        Cliente cliente = clienteDao.buscarClientePorEmail(email);
-//
-//        cliente.setCategoria(categoria);
-//        cliente.setTempoMeta(tempoMeta);
-//        clienteDao.formularioObjetivo(email,categoria,tempoMeta);
-//        return Constantes.MSG_FORMULARIO_SUCESSO;
-//    }
-//
-//    public JwtData obterDadosDoUsuario(String token) {
-//        String email = jwtUtils.getUserNameFromJwtToken(token);
-//        Cliente cliente = clienteDao.buscarClientePorEmail(email);
-//
-//        if (cliente != null) {
-//            JwtData jwtData = new JwtData();
-//            jwtData.setEmail(cliente.getEmail());
-//            jwtData.setCategoria(cliente.getCategoria());
-//            jwtData.setTempoMeta(cliente.getTempoMeta());
-//            jwtData.setPeso(cliente.getPeso());
-//            jwtData.setAltura(cliente.getAltura());
-//            jwtData.setDataNascimento(new Date(cliente.getDataNascimento().getTime()));
-//            jwtData.setGenero(cliente.getGenero());
-//            return jwtData;
-//        } else {
-//            throw new RuntimeException("Cliente não encontrado com o email: " + email);
-//        }
-//    }
+////    @Autowired
+////    private AuthenticationManager authenticationManager;
+////
+////    @Autowired
+////    private JwtUtils jwtUtils;
+////
+////    @Autowired
+////    private PasswordEncoder passwordEncoder;
+////
+////    public String criarCliente(String nome, String email, String genero, Double peso, Double pesoDesejado, Double altura, Date dataNascimento, String senha, String categoria, String tempoMeta){
+////
+////        if (!EmailValidador.isValidEmail(email)) {
+////            throw new EmailException();
+////        }
+////
+////        if (!SenhaValidador.isValidPassword(senha)) {
+////            throw new SenhaException();
+////        }
+////
+////        LocalDate dataNascimentoLocalDate = calculoIdade.convertToLocalDate(dataNascimento);
+////        int idade = calculoIdade.calcularIdade(dataNascimentoLocalDate);
+////        if (idade < 18) {
+////            throw new DataException();
+////        }
+////
+////        //Codifica a senha antes de salvar no banco de dados
+////        String encodedPassword = passwordEncoder.encode(senha);
+////
+////        clienteDao.criarCliente(nome, email, genero, peso, pesoDesejado, altura, dataNascimento, encodedPassword, categoria , tempoMeta);
+////        return Constantes.MSG_CRIACAO_CLIENTE_SUCESSO;
+////    }
+////
+////    /**
+////     * Autentica o usuário utilizando Spring Security e retorna um token JWT
+////     *
+////     * @param email o email do usuário
+////     * @param senha a senha do usuário
+////     * @return um token JWT válido ou null se a autenticação falhar
+////     */
+////    public String login(String email, String senha) {
+////        Authentication authentication = authenticationManager.authenticate(
+////                new UsernamePasswordAuthenticationToken(email, senha));
+////
+////            //Define o contexto de segurança com a autenticação bem-sucedida
+////        SecurityContextHolder.getContext().setAuthentication(authentication);
+////
+////        //Gera o token JWT utilizando as informaçoes do usuário autenticado
+////        String jwt = jwtUtils.generateJwtToken(authentication);
+////
+////        return jwt;
+////    }
+////
+////    public String alterarPeso(String email, double novoPeso) {
+////        Cliente cliente = clienteDao.buscarClientePorEmail(email);
+////
+////        cliente.setPeso(novoPeso);
+////        clienteDao.atualizarPesoCliente(email, novoPeso);
+////        return Constantes.MSG_PESO_ALTERADO_SUCESSO;
+////    }
+////
+////    public String formularioObjetivo(String email, String categoria, String tempoMeta) {
+////        Cliente cliente = clienteDao.buscarClientePorEmail(email);
+////
+////        cliente.setCategoria(categoria);
+////        cliente.setTempoMeta(tempoMeta);
+////        clienteDao.formularioObjetivo(email,categoria,tempoMeta);
+////        return Constantes.MSG_FORMULARIO_SUCESSO;
+////    }
+////
+////    public JwtData obterDadosDoUsuario(String token) {
+////        String email = jwtUtils.getUserNameFromJwtToken(token);
+////        Cliente cliente = clienteDao.buscarClientePorEmail(email);
+////
+////        if (cliente != null) {
+////            JwtData jwtData = new JwtData();
+////            jwtData.setEmail(cliente.getEmail());
+////            jwtData.setCategoria(cliente.getCategoria());
+////            jwtData.setTempoMeta(cliente.getTempoMeta());
+////            jwtData.setPeso(cliente.getPeso());
+////            jwtData.setAltura(cliente.getAltura());
+////            jwtData.setDataNascimento(new Date(cliente.getDataNascimento().getTime()));
+////            jwtData.setGenero(cliente.getGenero());
+////            return jwtData;
+////        } else {
+////            throw new RuntimeException("Cliente não encontrado com o email: " + email);
+////        }
+////    }
 //    public double calcularGETSalvar(String email, String categoria, String tempoMeta, String genero, double peso, double altura, Date dataNascimento, String categoriaAtividade) {
 //        double tmb = calcularTaxaMetabolica(genero, peso, altura, dataNascimento);
 //        double get = calcularGET(tmb, categoriaAtividade);
